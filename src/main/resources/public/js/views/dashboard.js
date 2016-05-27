c20cfcfc13a54fadf874ffb9c69316deade5b555
wikla <wikla.veecla@gmail.com>
2016-05-27 14:43:56 +0200

    first model and fixes

--- src/main/resources/public/js/views/dashboard.js
+++ src/main/resources/public/js/views/dashboard.js
@@ -2,12 +2,30 @@ define([
     'jquery',
     'underscore',
     'backbone',
+    'models/attender',
     'text!templates/dashboard.html'
-], function($, _, Backbone, dashboardTemplate){
+], function($, _, Backbone, Attender, dashboardTemplate){
     var DashboardView = Backbone.View.extend({
 
+        model: Attender,
+
+        initialize: function() {
+            this.model = new Attender({username: "veecla"});
+        },
+
         render: function(){
-            this.$el.html(_.template(dashboardTemplate));
+
+            this.model.fetch({
+                success: function (model, response, options) {
+                    this.renderView();
+                }.bind(this)
+            });
+        },
+
+        renderView: function() {
+            this.$el.html(_.template(dashboardTemplate, {
+                model: this.model
+            }));
         }
     });
     return DashboardView;
