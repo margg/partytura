cf340434a51c864f0b40497f432a9cf7cda45da4
Malgorzata Salawa <mksalawa@gmail.com>
2016-06-20 00:47:57 +0200

    adjust home controller, add views

--- src/main/java/pl/edu/agh/tai/partytura/web/config/HomeController.java
+++ src/main/java/pl/edu/agh/tai/partytura/web/config/HomeController.java
@@ -10,13 +10,28 @@ import org.springframework.social.twitter.api.Twitter;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.edu.agh.tai.partytura.model.Attender;
+import pl.edu.agh.tai.partytura.model.Institution;
+import pl.edu.agh.tai.partytura.model.User;
+import pl.edu.agh.tai.partytura.persistence.AttenderRepository;
+import pl.edu.agh.tai.partytura.persistence.InstitutionRepository;
 
 import javax.inject.Provider;
 import java.security.Principal;
+import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class HomeController {
+  private static final Logger LOGGER = LoggerFactory.getLogger(HomeController.class);
+
   private final Provider<ConnectionRepository> connectionRepositoryProvider;
+
+  @Autowired
+  private AttenderRepository attenderRepository;
+
+  @Autowired
+  private InstitutionRepository institutionRepository;
 
   @Autowired
   public HomeController(Provider<ConnectionRepository> connectionRepositoryProvider) {
@@ -29,7 +44,30 @@ public class HomeController {
     Connection<Twitter> connection = connectionRepository.findPrimaryConnection(Twitter.class);
     UserProfile profile = connection.fetchUserProfile();
     model.addAttribute(profile);
-    return "home";
+
+    String username = profile.getUsername();
+    Optional<User> user = getUser(username);
+    return user.map(u -> {
+      model.addAttribute("user", u);
+      return u instanceof Attender ? "attenderDashboard" : "instDashboard";
+    }).orElse("/error");
   }
 
+  private Optional<User> getUser(String username) {
+    Optional<User> user = Optional.empty();
+    List<Attender> attenders = attenderRepository.findByUsername(username);
+    List<Institution> institutions = institutionRepository.findByUsername(username);
+    if (!attenders.isEmpty()) {
+      if (attenders.size() > 1) {
+        LOGGER.warn("Found {} users with ID = {}.", attenders.size(), username);
+      }
+      user = Optional.of(attenders.get(0));
+    } else if (!institutions.isEmpty()) {
+      if (institutions.size() > 1) {
+        LOGGER.warn("Found {} users with ID = {}.", institutions.size(), username);
+      }
+      user = Optional.of(institutions.get(0));
+    }
+    return user;
+  }
 }
